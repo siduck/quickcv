e673b166121d936362545181c042b27a8c50d175
siduck76 <siduckk76@protonmail.com>
2021-10-20 13:39:43 +0530

    added some sections in editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,7 @@
 import "../styles/editor.css";
+import React, { Component } from "react";
+
+import uniqid from "uniqid";
 
 function Input(props) {
   return <input type="text" placeholder={props.info} />;
@@ -9,27 +12,64 @@ function PersonalInfo() {
     <div className="personalInfo">
       <h3>Personal Information</h3>
 
-      <div>
-        <Input info="Name" />
-        <Input info="Title" />
-      </div>
-
-      <div>
-        <Input info="Phone number" />
-        <Input info="Email" />
-      </div>
-
+      <Input info="Name" />
+      <Input info="Title" />
+      <Input info="Phone number" />
+      <Input info="Email" />
       <Input info="Profile Image url" />
       <Input info="Address" />
-      <Input info="Description" />
     </div>
   );
+}
+
+function Skills(props) {
+  return (
+    <div>
+      {Array.from(Array(props.n)).map(() =>
+        <Input info="a skill" key={uniqid} />
+      )}
+    </div>
+  );
+}
+
+class About extends Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: 1,
+    };
+
+    this.addSkill = this.addSkill.bind(this);
+  }
+
+  addSkill() {
+    this.setState({
+      skills: this.state.skills + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="aboutMe">
+          <h3>About me</h3>
+          <Input info="Description" />
+
+          <h3>Skills</h3>
+          <Skills n={this.state.skills} />
+        </div>
+
+        <button onClick={this.addSkill}>add</button>
+      </div>
+    );
+  }
 }
 
 export default function Editor() {
   return (
     <div className="editor">
       <PersonalInfo />
+      <About />
     </div>
   );
 }
