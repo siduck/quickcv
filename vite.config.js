8c7f463082b699d155d80c83acc4cd7c5ce625fc
siduck <siduck@tutanota.com>
2021-11-01 06:59:11 +0530

    fix typo in vite config base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://siduck.github.io/cv-project/",
+  base: "./",
   plugins: [react()],
 });
