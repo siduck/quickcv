4d97b435d7874603bff94348ef73fe604802620c
siduck <siduck@tutanota.com>
2023-01-20 13:11:04 +0530

    add demo button
    clicking this will toggle etween setting dummy data into input fields and emptying of the inputfields when demo is not shown

--- src/components/modeSwitcher.tsx
+++ src/components/modeSwitcher.tsx
@@ -1,28 +1,50 @@
 import "iconify-icon";
 import { createSignal } from "solid-js";
+import { get_default_data, get_demo_data, setStore } from "../store";
 
-export const [editorMode, setEditorMode] = createSignal(false);
+export const [editorMode, setEditorMode] = createSignal(true);
+const [demoShown, setDemo] = createSignal(false);
 
 export default () => (
-  <div class="mb-10 [&_button]:rounded-full vertCentered bg-slate-7 dark:bg-black-2 w-fit p-2 rounded-full !gap-1 mx-auto">
-    <button
-      bg={editorMode() ? "slate-6 dark:black-3" : "dark:black-2"}
-      shadow={editorMode() ? "lg" : "none"}
-      text={editorMode()? "!white-1 dark:!white-2": "!slate-4"}
-      onclick={() => setEditorMode(true)}
-    >
-      <iconify-icon icon="ph:pencil-simple-fill"></iconify-icon>
-      Editor
-    </button>
+  <div mb-10 vertCentered mx-auto w-fit>
+    <div class="[&_button]:rounded-full vertCentered bg-slate-7 dark:bg-black-2 w-fit p-2 rounded-full !gap-1">
+      <button
+        bg={editorMode() ? "slate-6 dark:black-3" : "dark:black-2"}
+        shadow={editorMode() ? "lg" : "none"}
+        text={editorMode() ? "!white-1 dark:!white-2" : "!slate-4"}
+        onclick={() => setEditorMode(true)}
+      >
+        <iconify-icon icon="ph:pencil-simple-fill"></iconify-icon>
+        Editor
+      </button>
+
+      <button
+        bg={!editorMode() ? "slate-5 dark:black-3" : "dark:black-2"}
+        shadow={!editorMode() ? "lg" : "none"}
+        onclick={() => setEditorMode(false)}
+        text={!editorMode() ? "!white-1 dark:!white-2" : "!slate-4"}
+      >
+        <iconify-icon icon="ic:round-grid-view"></iconify-icon>
+        Viewer
+      </button>
+    </div>
 
     <button
-      bg={!editorMode() ? "slate-5 dark:black-3" : "dark:black-2"}
-      shadow={!editorMode() ? "lg" : "none"}
-      onclick={() => setEditorMode(false)}
-      text={!editorMode()? "!white-1 dark:!white-2": "!slate-4"}
+      p-4
+      onclick={() => {
+        setDemo(!demoShown());
+        setEditorMode(!demoShown());
+        setStore(demoShown() ? get_demo_data() : get_default_data());
+      }}
     >
-      <iconify-icon icon="ic:round-grid-view"></iconify-icon>
-      Viewer
+      <iconify-icon
+        width="20"
+        icon={`material-symbols:${
+          demoShown() ? "check-circle-rounded" : "circle-outline"
+        }`}
+      >
+      </iconify-icon>
+      Demo
     </button>
   </div>
 );
--- src/store.tsx
+++ src/store.tsx
@@ -1,79 +1,81 @@
 import { createStore } from "solid-js/store";
 
-export const default_data = {
-  "full name": "",
-  designation: "",
-  phone: "",
-  address: "",
-  "email address": "",
-  "github username": "",
-  "career objective": "",
-  about: "",
+export const get_default_data = () => {
+  return {
+    "full name": "",
+    designation: "",
+    phone: "",
+    address: "",
+    "email address": "",
+    "github username": "",
+    "career objective": "",
+    about: "",
 
-  education: [
-    { institution: "", qualification: "", time: "" },
-  ],
+    education: [
+      { institution: "", qualification: "", time: "" },
+    ],
 
-  "work experience": [
-    { company: "", position: "", time: "" },
-  ],
+    "work experience": [
+      { company: "", position: "", time: "" },
+    ],
 
-  skills: [""],
-  interests: [""],
+    skills: [""],
+    interests: [""],
+  };
 };
 
-export const [store, setStore] = createStore(default_data);
+export const get_demo_data = () => {
+  return {
+    "full name": "sidhanth rathod",
+    designation: "frontend web developer",
+    phone: "+91 9701611257",
+    address: "H-No: 1-18/389/49 Brahmanwadi, Begumpet, Hyderabad-500016",
+    "email address": "siduck@tutanota.com",
+    "github username": "siduck",
+    "career objective":
+      "To obtain a reliable and challenging frontend web developer's position at a reputed firm & building beautiful UI/UX interfaces whilst improving my skills",
+    about: "frontend web developer",
 
-const demo_data = {
-  "full name": "sidhanth rathod",
-  designation: "frontend web developer",
-  phone: "+91 9701611257",
-  address: "H-No: 1-18/389/49 Brahmanwadi, Begumpet, Hyderabad-500016",
-  "email address": "siduck@tutanota.com",
-  "github username": "siduck",
-  "career objective":
-    "To obtain a reliable and challenging frontend web developer's position at a reputed firm & building beautiful UI/UX interfaces whilst improving my skills",
-  about: "frontend web developer",
+    education: [
+      {
+        institution: "sri chaitainya junior college",
+        qualification: "intermediate(11-12th grade)",
+        time: "2017-2019",
+      },
+      {
+        institution: "sister nividita school",
+        qualification: "10th grade (SSC)",
+        time: "2017",
+      },
+    ],
 
-  education: [
-    {
-      institution: "sri chaitainya junior college",
-      qualification: "intermediate(11-12th grade)",
-      time: "2017-2019",
-    },
-    {
-      institution: "sister nividita school",
-      qualification: "10th grade (SSC)",
-      time: "2017",
-    },
-  ],
+    "work experience": [
+      {
+        company: "jamesmccallumconsulting",
+        position: "Remote frontend react developer (freelancing)",
+        time: "2022 Oct - 2023 Jan",
+      },
+    ],
 
-  "work experience": [
-    {
-      company: "jamesmccallumconsulting",
-      position: "Remote frontend react developer (freelancing)",
-      time: "2022 Oct - 2023 Jan",
-    },
-  ],
+    skills: [
+      "html skill-icons:html",
+      "css skill-icons:css",
+      "javascript skill-icons:javascript",
+      "typescript skill-icons:typescript",
+      "react logos:react",
+      "tailwind css logos:tailwindcss-icon",
+      "unocss simple-icons:unocss",
+      "solid JS logos:solidjs-icon",
+    ],
 
-  skills: [
-    "html skill-icons:html",
-    "css skill-icons:css",
-    "javascript skill-icons:javascript",
-    "typescript skill-icons:typescript",
-    "react logos:react",
-    "tailwind css logos:tailwindcss-icon",
-    "unocss simple-icons:unocss",
-    "solid JS logos:solidjs-icon",
-  ],
-
-  interests: [
-    "tweaking neovim skill-icons:neovim-dark",
-    "Gnu / artix linux simple-icons:artixlinux",
-    "lua scripting file-icons:lua",
-    "Ricing linux bi:palette2",
-    "shell scripting file-icons:terminal"
-  ],
+    interests: [
+      "tweaking neovim skill-icons:neovim-dark",
+      "Gnu / artix linux simple-icons:artixlinux",
+      "lua scripting file-icons:lua",
+      "Ricing linux bi:palette2",
+      "shell scripting file-icons:terminal",
+    ],
+  };
 };
 
-setStore(demo_data);
+export const [store, setStore] = createStore(get_default_data());
