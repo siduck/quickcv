f2a5ef942526f1f071d915ed0ea401b6679b5a9b
siduck <siduck@tutanota.com>
2023-01-21 13:03:36 +0530

    add opegraph & some meta tsgs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   return (
     <div
       class="vertCentered justify-between [&_iconify-icon]:text-xl [&_button]:rounded-full"
-      p="y-2 b-5 md:y-5"
+      p="y-2 b-5 md:y-5 md:t-2"
     >
       {/* Branding */}
       <h3 vertCentered text='blue-6 dark:blue-5' class="text-2xl my-0">
